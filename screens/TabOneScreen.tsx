c85afb2888d4b79f167583f62046b0d705001082
lifecoderua <lifecoder.ua@gmail.com>
2020-10-16 17:02:06 +0300

    realm integration - anonymous init

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,10 +1,32 @@
 import * as React from 'react';
+import * as Realm from 'realm-web';
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+const app: Realm.App = new Realm.App({ id: "medicare-vvmvo" });
+
+async function createAnonymousUser() {
+  const credentials = Realm.Credentials.anonymous();
+  try {
+    // Authenticate the user
+    const user: Realm.User = await app.logIn(credentials);
+    console.log('Realm anon user created:', user);
+    // `App.currentUser` updates to match the logged in user
+    // assert(user.id === app.currentUser.id)
+    return user
+  } catch(err) {
+    console.error("Failed to log in", err);
+  }
+}
+
+// TODO: anonymous user auth is not supported
+createAnonymousUser();
+
+
 export default function TabOneScreen() {
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
